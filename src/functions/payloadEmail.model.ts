9e843f78321df85a24d7d681cccfb5cb000ac40f
kevinkeyssx <keyss1700@gmail.com>
2026-04-24 19:27:08 -0400

    models updated

--- src/functions/payloadEmail.model.ts
+++ src/functions/payloadEmail.model.ts
@@ -1,5 +1,45 @@
+export enum Priority {
+    NORMAL = "NORMAL",
+    HIGH   = "HIGH"
+}
+
+
 export interface PayloadEmail {
-    email       : string;
-    template    : string;
-    subject     : string;
+    student        : PayloadStudent;
+    templateId     : string;
+    subject        : string;
+    notificationId : string;
+    priority?      : Priority | null;
+    cc?            : string[] | null;
+    bcc?           : string[] | null;
+}
+
+
+export interface PayloadStudent {
+    email   : string;
+    name?   : string | null;
+}
+
+
+export interface PayloadRecurrent {
+    workflowId : string;
+    cronRule   : string;
+}
+
+export interface Template {
+    id : string;
+    content : string;
+}
+
+
+export interface Workflow {
+    id              : string;
+    name            : string;
+    students        : PayloadStudent[];
+    notificationId  : string;
+    subject         : string;
+    cc?             : string[] | null;
+    bcc?            : string[] | null;
+    status          : 'ACTIVE' | 'INACTIVE';
+    template        : Template;
 }
